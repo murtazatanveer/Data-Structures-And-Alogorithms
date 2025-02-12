6713f4acb98425d1c0f6ed28dea2c893a9cd457b
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-02-12 11:41:13 +0500

    Implement Stack Using Queues.

--- Stack/leetCode.java
+++ Stack/leetCode.java
@@ -1,5 +1,5 @@
 package Stack;
-import java.util.Stack;
+import java.util.*;
 
 public class leetCode {
     
@@ -264,57 +264,6 @@ private char precedence(char op){
     return 'b';   
 }
 
-/* 32. Longest Valid Parentheses
-Hard
-Topics
-Companies
-Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
-substring
-.
-
- 
-
-Example 1:
-
-Input: s = "(()"
-Output: 2
-Explanation: The longest valid parentheses substring is "()".
-Example 2:
-
-Input: s = ")()())"
-Output: 4
-Explanation: The longest valid parentheses substring is "()()".
-Example 3:
-
-Input: s = ""
-Output: 0
- 
-
-Constraints:
-
-0 <= s.length <= 3 * 104
-s[i] is '(', or ')'. */
-
-//  int longestValidParentheses(String s) {
-
-//     if (s.isEmpty() || s==null) {
-//         return 0;
-//     }
-
-//     Stack<Character> stack = new Stack<Character>();
-//     stack.push(s.charAt(0));
-//     int len = 0;
-//     for (int i = 1; i < s.length(); i++) {
-//         if (s.charAt(i)==')' && !stack.isEmpty() && stack.peek()=='(' ){
-//             stack.pop();
-//             len+=2;
-//         }else{
-//             stack.push(s.charAt(i));
-//         }
-//     }
-//     return len;
-// }
-// incomplete
 
 
 /* 921. Minimum Add to Make Parentheses Valid
@@ -424,6 +373,87 @@ public String removeStars(String s) {
     }
     return new String(chrs);
 }
+
+/* 225. Implement Stack using Queues
+Easy
+Topics
+Companies
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, top, and empty.
+All the calls to pop and top are valid.
+ 
+
+Follow-up: Can you implement the stack using only one queue? */
+
+class MyStack {
+
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>(); 
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for (int i = 1; i < q.size(); i++) {
+            q.add(q.remove());
+        }
+    }
+    
+    public int pop() {
+        return q.remove();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
 
 public static void main(String[] args) {
     leetCode ob = new leetCode();
