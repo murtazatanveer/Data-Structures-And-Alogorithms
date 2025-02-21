c1ffb2efc3f1c961cfb96967a56188bb5473c7aa
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-02-21 16:11:10 +0500

    Recursion.

---
+++ Recursion/recursion.java
@@ -0,0 +1,140 @@
+package Recursion;
+import java.util.*;
+
+public class recursion {
+    public static void main(String[] args) {
+        recursion ob = new recursion();
+
+        System.out.println(ob.BinarySearch(new int[]{5,10,15,20,25,30,35,40,45,50}, 0, 9, 20));
+
+        int []arr = {20,40,25,50,5,35,15,45,10,30};
+
+        ob.bubbleSort(arr, 9);
+      System.out.println(Arrays.toString(arr));
+
+      System.out.println(ob.reverseNumber(12345,0));
+      System.out.println(ob.sumFromStartToEnd(5, 100));
+      System.out.println(ob.productFromStartToEnd(5, 10));
+      System.out.println(ob.sumOfDigits(12345));
+      System.out.println(ob.productOfDigits(12345));
+      System.out.println(ob.isPalindrome(new int[]{1,5,9,9,5,1}, 0, 5));
+
+    System.out.println(ob.countZeros(905600200, 0));
+
+    }
+
+    void printNumbers(int start , int end){
+
+        if (start>end) {
+            return;
+        }
+
+        System.out.println(start);
+        printNumbers(start+1, end);
+    }
+
+    int reverseNumber(int num ,int rev){
+
+        if (num==0) {
+            return rev;
+        }
+
+        return reverseNumber(num/10,(rev*10)+(num%10));
+
+     }
+   
+    int BinarySearch(int []arr , int start , int end , int target){
+
+        if (start>end) {
+            return -1;
+        }
+
+        int mid = (start+end)/2;
+
+        if (target>arr[mid]) {
+            return BinarySearch(arr, mid+1, end, target);
+        }else if(target<arr[mid]){
+            return BinarySearch(arr, start, mid-1, target);
+        }   
+            return mid;
+                  
+     }
+
+     void bubbleSort(int []arr , int i){
+
+        if (i==0) {
+            return;
+        }
+
+        for (int j = 1; j <= i; j++) {
+            if (arr[j-1]>arr[j]) {
+
+                int temp= arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+            }
+        }
+
+        bubbleSort(arr, i-1);
+     }
+
+
+     int sumFromStartToEnd(int start , int end){
+
+        if (start>=end) {
+            return end;
+        }
+
+        return start+sumFromStartToEnd(start+1, end);
+     }
+
+     int productFromStartToEnd(int start , int end){
+
+        if (start>=end) {
+            return end;
+        }
+
+        return start*productFromStartToEnd(start+1, end);
+     }
+
+     int sumOfDigits(int num){
+
+        if (num==0) {
+            return 0;
+        }
+
+        return (num%10)+sumOfDigits(num/10);
+     }
+
+     int productOfDigits(int num){
+
+        if (num==0) {
+            return 1;
+        }
+        
+        return (num%10)*productOfDigits(num/10);
+     }
+
+
+     boolean isPalindrome(int [] arr , int start , int end){
+
+       if (start>=end) {
+        return true;
+       }
+
+       else if(arr[start]==arr[end]){
+        return isPalindrome(arr, start+1, end-1);
+       }
+
+       return false;
+
+     }
+
+     int countZeros(int num , int count){
+
+        if (num==0)return count;
+        
+        return num%10==0 ? countZeros(num/10, count+1) : countZeros(num/10, count);
+
+     }
+}
--- Sorting Algorithms/Bubble_Sort.java
+++ Sorting Algorithms/Bubble_Sort.java
@@ -5,7 +5,7 @@ class bubbleSort{
 
     void basicBubbleSort(int []arr){
 
-        for (int i = arr.length-1; i >= 0; i--) {
+        for (int i = arr.length-1; i >= 1; i--) {
 
             for(int j = 1;j<=i;j++){
 
