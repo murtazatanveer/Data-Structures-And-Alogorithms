f6daacf42ea52b32a5a3ba413750bfad682a8c6b
Murtaza_Tanveer <murtaza.tanveer488@gmail.com>
2025-02-25 11:14:08 +0500

    Balanced Binary Tree.(Leetcode Problem No 110)

--- Trees/TreesLeetcode.java
+++ Trees/TreesLeetcode.java
@@ -395,5 +395,31 @@ Given the root of a binary tree, check whether it is a mirror of itself (i.e., s
         return true;
     }
 
+    // Leetcode Problem No 110. Balanced Binary Tree
+    public boolean isBalanced(TreeNode root) {
+        return checkBalance(root)!=-1;
+    }
+
+    private int checkBalance(TreeNode root){
+
+        if (root==null) {
+            return 0;
+        }
+
+        int left = checkBalance(root.left);
+        if (left==-1) return -1;     
+        int right = checkBalance(root.right);
+        if (right==-1) return -1;
+            
+        if (Math.abs(left-right)>1) {
+            return -1;
+        }
+
+        if (left>right) {
+            return ++left;
+        }
+        return ++right;
+    }
+
 }
 
